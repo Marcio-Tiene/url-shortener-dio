50db218629ae27d3c652c7f15bc038f0e7cd850a
Marcio Tiene <marcio.ricardo@mettzer.com>
2021-12-22 12:28:07 -0300

    adjust mongo to use docker

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,5 +1,5 @@
 export const config = {
 	API_URL: 'http://localhost:5000',
 	MONGO_CONNECTION:
-		'mongodb+srv://<user>:<password>@<database>.yptlq.mongodb.net/url-shortener-dio?retryWrites=true&w=majority',
+		'mongodb://localhost:27017/url-shortener-dio',
 }
